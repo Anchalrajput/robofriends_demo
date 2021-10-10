029d058b961c6c13fd5bf68c693f27b528d211ee
Anchal Rajput <anchal.rajput99@gmail.com>
2021-10-10 22:43:28 +0530

    trying to upload folder

---
+++ src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [
+{
+	id:1,
+	name: 'Aanchal Rana',
+	username:'Mona',
+	email:'anchal.rajput99@gmail.com'
+},
+{
+	id:2,
+	name:'Ankit Rana',
+	username:'Ankit',
+	email:'ankit@gmail.com'
+},
+{
+	id:3,
+	name:'Rupa Rana',
+	username:'Rupa',
+	email:'rupa@gmail.com'
+},
+{
+	id:4,
+	name:'Shalini Rana',
+	username:'Shalu',
+	email:'shalini@gmail.com'
+},
+{
+	id:5,
+	name:'Sunil Rana',
+	username:'Sunil',
+	email:'sunil@gmail.com'
+}]
